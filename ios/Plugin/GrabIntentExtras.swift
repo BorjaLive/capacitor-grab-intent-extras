eb3295b02febce9694396877ba6f9eb3effd0213
BorjaLive <borjainlive@gmail.com>
2023-03-15 17:14:29 +0100

    Ahora funciona en iOS

--- ios/Plugin/GrabIntentExtras.swift
+++ ios/Plugin/GrabIntentExtras.swift
@@ -1,7 +1,33 @@
 import Foundation
+import Capacitor
 
-@objc public class GrabIntentExtras: NSObject {
-    @objc public func getIntentExtras() -> String {
-        return "Not implemented yet";
+@objc public class GrabIntentExtras: NSObject, UNUserNotificationCenterDelegate {
+    public weak var plugin: CAPPlugin?
+    private var storedExtra: [String : Any]? = nil
+    
+    @objc public func getIntentExtras() -> [String : Any]? {
+        let extras = self.storedExtra
+        self.storedExtra = nil
+        return extras;
+    }
+    
+    @objc public func emitExtras(extras: [AnyHashable : Any]) {
+        guard let pExtras = extras as NSDictionary? as? [String: Any] else { return }
+        if self.plugin?.hasListeners("intentExtras") == true {
+            self.plugin?.notifyListeners("intentExtras", data: pExtras)
+        } else {
+            self.storedExtra = pExtras
+        }
+    }
+    
+    //Mostrar la notificacion tal cual
+    public func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound])
+    }
+
+    //Avisar del click en la notificacion
+    public func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        self.emitExtras(extras: response.notification.request.content.userInfo)
+        completionHandler()
     }
 }
--- ios/Plugin/GrabIntentExtrasPlugin.swift
+++ ios/Plugin/GrabIntentExtrasPlugin.swift
@@ -1,18 +1,31 @@
 import Foundation
 import Capacitor
 
-/**
- * Please read the Capacitor iOS Plugin Development Guide
- * here: https://capacitorjs.com/docs/plugins/ios
- */
 @objc(GrabIntentExtrasPlugin)
 public class GrabIntentExtrasPlugin: CAPPlugin {
+    public static var launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
     private let implementation = GrabIntentExtras()
-
+    
+    override public func load() {
+        UNUserNotificationCenter.current().delegate = self.implementation
+        self.implementation.plugin = self
+        if let launchOptions = GrabIntentExtrasPlugin.launchOptions as? [String: Any] {
+            if let launchExtras = launchOptions["UIApplicationLaunchOptionsRemoteNotificationKey"] as? [AnyHashable : Any] {
+                self.implementation.emitExtras(extras: launchExtras)
+            }
+        }
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
     @objc func getIntentExtras(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "extras": implementation.getIntentExtras()
-        ])
+        let extras = implementation.getIntentExtras();
+        if extras != nil {
+            call.resolve(extras!);
+        } else {
+            call.resolve();
+        }
     }
 }
